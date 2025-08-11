acf0fc8957b7784bdcc9e2e944129d40c01f0eab
jinoc <choiae369@gmail.com>
2025-08-11 21:27:28 +0900

    feat: 노다시 직업 업데이트 추가

--- app/components/Update.tsx
+++ app/components/Update.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Update() {
-  const updateDate = '(2025.07.14)';
+  const updateDate = '(2025.08.11)';
   return (
     <>
       <span className=" text-sm font-normal">업데이트: {updateDate}</span>
