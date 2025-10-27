c7c684bb6b5fcd0da78cddfffe2d595c0d15b945
jinoc <choiae369@gmail.com>
2025-10-27 20:39:15 +0900

    feat: 업데이트 날짜 수정

--- app/components/Update.tsx
+++ app/components/Update.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Update() {
-  const updateDate = '(2025.08.11)';
+  const updateDate = '(2025.10.27)';
   return (
     <>
       <span className=" text-sm font-normal">업데이트: {updateDate}</span>
