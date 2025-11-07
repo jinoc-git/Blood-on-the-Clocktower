4d61e1db7c73c08f0d8418b07dc83b76822c444c
jinoc <choiae369@gmail.com>
2025-11-07 20:52:18 +0900

    feat: 팡키 직업 업데이트

--- app/components/Update.tsx
+++ app/components/Update.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default function Update() {
-  const updateDate = '(2025.10.27)';
+  const updateInfo = '팡키 수정';
+  const updateDate = '(2025.11.07)';
   return (
     <>
-      <span className=" text-sm font-normal">업데이트: {updateDate}</span>
+      <span className=" text-sm font-normal">
+        업데이트: {updateInfo} {updateDate}
+      </span>
     </>
   );
 }
